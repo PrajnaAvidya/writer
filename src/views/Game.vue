3eef53f34c27237f487adc4cc16fa579c5a2ae4b
Rafiq Premji <rafiq@artard.com>
2019-01-11 18:43:40 -0700

    removed boilerplate and added a button/counter

---
+++ src/views/Game.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="game">
+    <div class="words">
+    Words: {{ words }}
+    </div>
+    <button @click="write">Write</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'game',
+  data: () => ({
+    words: 0,
+  }),
+  methods: {
+    write() {
+      this.words += 1;
+    },
+  },
+};
+</script>
