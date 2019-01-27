0b2ddd64a27adc9ea49da48aa9b36b5e27118681
Rafiq <r@neat.af>
2019-01-26 18:17:18 -0700

    todo

--- src/main.js
+++ src/main.js
@@ -3,14 +3,11 @@ import 'bulma-tooltip/dist/css/bulma-tooltip.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import Vue from 'vue';
-import VueSlideBar from 'vue-slide-bar';
 
 import Game from '@/Game.vue';
 import router from '@/router';
 import store from '@/store';
 import '@/filters';
-
-Vue.component('VueSlideBar', VueSlideBar);
 
 Vue.config.productionTip = false;
 
