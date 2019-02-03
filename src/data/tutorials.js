c608fcd5e2f39aeab240802a11eb62ee748e981e
Rafiq <r@neat.af>
2019-02-03 14:11:15 -0700

    placeholders for more tutorials

--- src/data/tutorials.js
+++ src/data/tutorials.js
@@ -1,4 +1,4 @@
-// TODO conditions/data for tutorial modals
+// conditions/data for tutorial modals
 
 export default [
   {
@@ -7,13 +7,17 @@ export default [
     },
     delay: 30,
     text: 'I didn\'t realize this would be SO HARD. It\'s taking forever just to write a few words! Maybe drinking a coffee would help...',
-    // TODO some kind of way to indicate that the coffee button unlocks & is highlighted
+    // TODO reveal/highlight caffeine
   },
   {
     unlock: {
       caffeine: true,
     },
     delay: 60,
-    text: 'Woah that feels <i>GOOD</i> and makes me write much faster!',
+    text: 'Woah that feels <i>GOOD</i> and makes me write much faster! Now that I\'ve written some words maybe I can sell them at the writing agency down the street...',
+    // TOOD reveal/highlight jobs
   },
+  // TODO introduce workers
+  // TODO introduce urgent jobs
+  // TODO introduce upgrades
 ];
