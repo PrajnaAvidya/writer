a0f03038162340eeddf5a3b5a96fb7035f0d1d75
Rafiq <r@neat.af>
2019-02-15 17:34:31 -0700

    add glow/pulse effect to rebirth button

--- src/views/Rebirth.vue
+++ src/views/Rebirth.vue
@@ -15,6 +15,7 @@
       </p>
       <a
         class="button is-warning"
+        :class="{ glow: checkDebug('rebirth') || milestones.gte(milestonesNeeded), pulse: checkDebug('rebirth') || milestones.gte(milestonesNeeded) }"
         :disabled="!checkDebug('rebirth') && milestones.lt(milestonesNeeded)"
         @click="confirmRebirth()"
       >
