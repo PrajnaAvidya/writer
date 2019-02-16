844925539ddf5e850509555313d2e5bb3a77f6df
Rafiq <r@neat.af>
2019-02-15 21:38:55 -0700

    changed back lol

--- src/utils/generateJob.js
+++ src/utils/generateJob.js
@@ -5,31 +5,31 @@ import randomJobName from '@/utils/randomJobName';
 const jobTypes = {
   1: {
     name: 'Tiny',
-    rewardRange: [300, 400],
+    rewardRange: [200, 300],
     wordRange: [10, 30],
     minimumWords: 100,
   },
   2: {
     name: 'Short',
-    rewardRange: [400, 500],
+    rewardRange: [300, 400],
     wordRange: [30, 90],
     minimumWords: 200,
   },
   3: {
     name: 'Medium',
-    rewardRange: [500, 600],
+    rewardRange: [400, 500],
     wordRange: [180, 600],
     minimumWords: 1000,
   },
   4: {
     name: 'Long',
-    rewardRange: [600, 700],
+    rewardRange: [500, 600],
     wordRange: [900, 2400],
     minimumWords: 2000,
   },
   5: {
     name: 'Epic',
-    rewardRange: [700, 800],
+    rewardRange: [600, 700],
     wordRange: [3600, 9000],
     minimumWords: 10000,
   },
