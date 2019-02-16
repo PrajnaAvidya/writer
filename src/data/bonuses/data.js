88d00cbfc48ab5a047b7661a4c1a60df16b01f73
Rafiq <r@neat.af>
2019-02-15 17:14:42 -0700

    bonus skill rework

--- src/data/bonuses/data.js
+++ src/data/bonuses/data.js
@@ -9,19 +9,19 @@ export default [
   {
     name: 'Third Job Slot',
     description: 'Unlocks another job slot',
-    cost: 100,
+    cost: 75,
     type: 'jobSlot',
   },
   {
     name: 'Fourth Job Slot',
     description: 'Unlocks yet another job slot',
-    cost: 300,
+    cost: 150,
     type: 'jobSlot',
   },
   {
     name: 'Fifth Job Slot',
     description: 'Unlocks the final job slot!',
-    cost: 1000,
+    cost: 300,
     type: 'jobSlot',
   },
   // cooldowns
@@ -34,102 +34,69 @@ export default [
   {
     name: 'HURRY, COOLDOWNS!',
     description: 'Increase effectiveness of clicking job/caffeine cooldown bars even more',
-    cost: 1000,
+    cost: 200,
     type: 'hurryAmount',
-  },
-  // coffee wps
-  {
-    name: 'Good Coffee',
-    description: 'Permanently doubles the word generating effect of caffeine',
-    cost: 100,
-    type: 'caffeineWordMultiplier',
-  },
-  {
-    name: 'Really Good Coffee',
-    description: 'Permanently doubles the word generating effect of caffeine',
-    cost: 500,
-    type: 'caffeineWordMultiplier',
-  },
-  {
-    name: 'Great Coffee',
-    description: 'Permanently doubles the word generating effect of caffeine',
-    cost: 1000,
-    type: 'caffeineWordMultiplier',
-  },
-  {
-    name: 'Amazing Coffee',
-    description: 'Permanently doubles the word generating effect of caffeine',
-    cost: 2500,
-    type: 'caffeineWordMultiplier',
-  },
-  {
-    name: 'Overpriced Coffee',
-    description: 'Permanently doubles the word generating effect of caffeine',
-    cost: 5000,
-    type: 'caffeineWordMultiplier',
   },
   // passive $
   {
     name: 'Easy Money',
     description: 'Recieve 1 cent for every word generated (includes job reward multipliers)',
-    cost: 150,
+    cost: 100,
     type: 'passiveMoney',
   },
   {
     name: 'EZ Money',
     description: 'Doubles passive money generation (includes job reward multiplier)',
-    cost: 250,
+    cost: 200,
     type: 'passiveMoney',
   },
   {
     name: 'Ballin\'',
     description: 'Doubles passive money generation (includes job reward multiplier)',
-    cost: 500,
+    cost: 300,
     type: 'passiveMoney',
   },
   {
     name: 'Trust Fund',
     description: 'Doubles passive money generation (includes job reward multiplier)',
-    cost: 450,
+    cost: 400,
     type: 'passiveMoney',
   },
   {
     name: 'Plutocracy',
     description: 'Doubles passive money generation (includes job reward multiplier)',
-    cost: 550,
+    cost: 500,
     type: 'passiveMoney',
   },
-  // TODO use these names for a new bonus
-  /*
+  // caffeine wps
   {
     name: 'Cappuccino',
-    description: 'Adds 100% of your worker words per second per click during caffeine boost',
-    cost: 200,
-    type: 'todo',
+    description: 'Permanently doubles the word generating effect of caffeine',
+    cost: 100,
+    type: 'caffeineWordMultiplier',
   },
   {
     name: 'Espresso',
-    description: 'Adds 100% of your worker words per second per click during caffeine boost',
-    cost: 1000,
-    type: 'todo',
+    description: 'Permanently doubles the word generating effect of caffeine',
+    cost: 200,
+    type: 'caffeineWordMultiplier',
   },
   {
     name: 'Latte',
-    description: 'Adds 100% of your worker words per second per click during caffeine boost',
-    cost: 2000,
-    type: 'todo',
+    description: 'Permanently doubles the word generating effect of caffeine',
+    cost: 300,
+    type: 'caffeineWordMultiplier',
   },
   {
     name: 'Mocha',
-    description: 'Adds 100% of your worker words per second per click during caffeine boost',
-    cost: 5000,
-    type: 'todo',
+    description: 'Permanently doubles the word generating effect of caffeine',
+    cost: 400,
+    type: 'caffeineWordMultiplier',
   },
   {
     name: 'Ristretto',
-    description: 'Adds 100% of your worker words per second per click during caffeine boost',
-    cost: 10000,
-    type: 'todo',
+    description: 'Permanently doubles the word generating effect of caffeine',
+    cost: 500,
+    type: 'caffeineWordMultiplier',
   },
-  */
 ];
