07cd0a230a1d2428490b9539fec5e2b6accf4db5
Rafiq <r@neat.af>
2019-03-01 19:05:35 -0700

    show 0% worker wps as <1

--- src/filters.js
+++ src/filters.js
@@ -5,6 +5,7 @@ import round from '@/utils/round';
 Vue.filter('round', value => round(value));
 Vue.filter('roundDecimal', value => `${round(value, { decimal: true })}`);
 Vue.filter('roundPositive', value => round(value, { alwaysPositive: true }));
+Vue.filter('roundPercent', value => round(value, { percentFormat: true }));
 Vue.filter('money', value => `$${round(value)}`);
 Vue.filter('moneyCents', value => `$${round(value, { decimal: true })}`);
 Vue.filter('orderCost', list => sortBy(list, item => parseInt(item.cost.toFixed(), 10)));
