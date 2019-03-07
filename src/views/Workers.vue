ea3eb94b4a9e88978d61675a570ee0eee7178820
Rafiq <r@neat.af>
2019-03-06 19:47:11 -0700

    show manager worker count on page

--- src/views/Workers.vue
+++ src/views/Workers.vue
@@ -33,7 +33,10 @@
         </div>
         <div class="stats-column">
           <span v-if="worker.quantity > 0">
-            <strong>{{ worker.pluralName }}: {{ worker.quantity | round }}</strong>
+            <strong>{{ worker.pluralName }}: {{ worker.quantity | round }}<span
+              v-if="worker.managerHired > 0"
+              class="manager-hired"
+            >+{{ worker.managerHired }}</span></strong>
             <br>
             Words per Second: {{ individualWorkerWps[worker.id] | round }} ({{ workerWpsPercent(worker) | roundPercent }}%)
           </span>
@@ -112,5 +115,8 @@ export default {
 }
 .stats-column {
   width: 310px;
+}
+.manager-hired {
+  color: darken($red, 20%);
 }
 </style>
