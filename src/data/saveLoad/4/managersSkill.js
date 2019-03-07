197cc1fd152f704365018ac52dff77cd55c948ed
Rafiq <r@neat.af>
2019-03-06 20:43:00 -0700

    fix bonus name

--- src/data/saveLoad/4/managersSkill.js
+++ src/data/saveLoad/4/managersSkill.js
@@ -27,19 +27,19 @@ export default [
     name: 'Organic Growth',
     description: 'Managers hire 2 workers at once',
     cost: 500,
-    type: 'managerUpgrade',
+    type: 'managerWorkers',
     unfoldingCondition: 'showManagers',
   },
   {
     name: 'Corporate Synergy',
     description: 'Managers hire 5 workers at once',
     cost: 750,
-    type: 'managerUpgrade',
+    type: 'managerWorkers',
   },
   {
     name: 'Paradigm Shift',
     description: 'Managers hire 10 workers at once',
     cost: 1000,
-    type: 'managerUpgrade',
+    type: 'managerWorkers',
   },
 ];
